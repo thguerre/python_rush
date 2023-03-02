55221b1fbde24472f3c32846d90845d195080178
Théo Guerre <guerre.theo@gmail.com>
2023-03-02 07:55:47 +0100

    chg - using array and better parsing to support negatives

--- 01_string_manipulation/main.py
+++ 01_string_manipulation/main.py
@@ -1,20 +1,20 @@
 
 # asks user input
-firstNumber = input("Give me a number ! $ ")
-secondNumber = input("Give me yet another number ! $ ")
+numbers = []
+numbers.append( input("Give me a number ! $ ") )
+numbers.append( input("Give me yet another number ! $ ") )
 
 # checks validity
-if (not firstNumber.isnumeric() or not secondNumber.isnumeric()):
-    print(f"\nOne of the entries ('{firstNumber}' or '{secondNumber}') isn't a number, process exiting !")
-    exit()
-
-# parse integers
-firstNumber = int(firstNumber)
-secondNumber = int(secondNumber)
+for i in range(2):
+    try:
+        numbers[i] = int(numbers[i])
+    except:
+        print(f"\nOne of the entries ('{numbers[0]}' or '{numbers[1]}') isn't a number, process exiting !")
+        exit()
 
 # some number and string manipulation !
-print("\nHere, i added both numbers together ! " + str(firstNumber + secondNumber))
+print("\nHere, i added both numbers together ! " + str(numbers[0] + numbers[1]))
 
 # some unreadable triple nested parenthesis ternary syntax !
-multiplied = firstNumber * secondNumber
-print("\nThen, tried to multiply them" + ((" "+str(multiplied)) if multiplied != 0 else ".. and it gave a zero :("))
+multiplied = numbers[0] * numbers[1]
+print("\nThen, tried to multiply them too" + ((" : "+str(multiplied)) if multiplied != 0 else ".. and it gave a zero :("))
